4b8fd89812f974873ec3a2d85050753ad3e051ff
Andre-John Mas <andrejohn.mas@gmail.com>
2016-04-23 15:04:50 -0400

    Initial Commit

---
+++ HelloMoodMetric/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HelloMoodMetric
+//
+//  Created by André-John Mas on 2016-04-23.
+//  Copyright © 2016 André-John Mas. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
