6f43884e950ca3fa259fd53915353a0fe2260c25
Andre-John Mas <andrejohn.mas@gmail.com>
2016-04-23 16:52:23 -0400

    Added iOS target, renamed some resources

---
+++ HelloMoodMetric-iOS/ViewController.swift
@@ -0,0 +1,114 @@
+//
+//  ViewController.swift
+//  HelloMoodMetric-iOS
+//
+//  Created by André-John Mas on 2016-04-23.
+//  Copyright © 2016 André-John Mas. All rights reserved.
+//
+
+import UIKit
+import CoreBluetooth
+
+class ViewController: UIViewController {
+    
+    let mmServiceUUID = CBUUID(string:"dd499b70-e4cd-4988-a923-a7aab7283f8e")
+    let streamingCharacteristicUUID = CBUUID(string:"a0956420-9bd2-11e4-bd06-0800200c9a66")
+    var centralManager : CBCentralManager?
+    var peripheral: CBPeripheral?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+        print("waiting for bluetooth to become available")
+        // Do any additional setup after loading the view.
+    }
+        
+    func connect(peripheral: CBPeripheral!) {
+        centralManager!.connectPeripheral(peripheral, options: nil)
+        
+        //peripheral.delegate = self
+        
+        // We need to have a reference to the peripheral because the connection
+        // attempt will be canceled when the peripheral is deallocated
+        self.peripheral = peripheral
+    }
+}
+
+extension ViewController:  CBCentralManagerDelegate {
+    
+    func centralManagerDidUpdateState(central: CBCentralManager) {
+        if central.state == .PoweredOn {
+            print("bluetooth enabled, scanning for rings")
+            // Start scanning for devices that support the Moodmetric service
+            centralManager!.scanForPeripheralsWithServices([mmServiceUUID], options: nil)
+        }
+    }
+    
+    func centralManager(central: CBCentralManager, didDiscoverPeripheral peripheral: CBPeripheral, advertisementData: [String : AnyObject], RSSI: NSNumber) {
+        
+        print("found a ring, connecting to it")
+        // Stop scanning
+        centralManager!.stopScan()
+        // Connect to the ring
+        connect(peripheral)
+    }
+    
+    func centralManager(central: CBCentralManager, didConnectPeripheral peripheral: CBPeripheral) {
+        print("connected, discovering services")
+        peripheral.delegate = self
+        peripheral.discoverServices([mmServiceUUID])
+    }
+}
+
+extension ViewController: CBPeripheralDelegate {
+    
+    func peripheral(peripheral: CBPeripheral, didDiscoverServices error: NSError?) {
+        // Loop discovered services to find the MM service
+        // (though there should be only one)
+        for service in peripheral.services! {
+            if service.UUID == mmServiceUUID {
+                print("discovered MM service")
+                // Once we have the service we still need to discover
+                // its characteristics
+                peripheral.discoverCharacteristics([streamingCharacteristicUUID], forService: service)
+                return
+            }
+        }
+    }
+    
+    func peripheral(peripheral: CBPeripheral, didDiscoverCharacteristicsForService service: CBService, error: NSError?) {
+        // Loop discovered characteristics to find the streaming characteristic
+        for characteristic in service.characteristics! {
+            if characteristic.UUID == streamingCharacteristicUUID {
+                print("discovered streaming characteristic, enabling notifications")
+                // To receive measurements from the ring we
+                // need to enable notifications
+                peripheral.setNotifyValue(true, forCharacteristic: characteristic)
+                return
+            }
+        }
+    }
+    
+    func peripheral(peripheral: CBPeripheral, didUpdateValueForCharacteristic characteristic: CBCharacteristic, error: NSError?) {
+        
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.timeZone = NSTimeZone(name: "UTC")
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let dateStr = dateFormatter.stringFromDate(NSDate())
+        
+        let data = characteristic.value
+        var payload = [UInt8](count: data!.length, repeatedValue: 0)
+        data!.getBytes(&payload, length:data!.length)
+        // Decode payload (status & mood metric)
+        let status = Int(payload[0])
+        let mm = Int(payload[1])
+        // Instant EDA (Electrodermal activity) is in payload bytes 2 and 3 in big-endian format
+        let instant = (Int(payload[2]) << 8) | Int(payload[3])
+        let ax = Double(payload[4])/255*4 - 2
+        let ay = Double(payload[5])/255*4 - 2
+        let az = Double(payload[6])/255*4 - 2
+        // Acceleration magnitude (g)
+        let a = sqrt(ax*ax + ay*ay + az*az)
+        print("\(dateStr) - ", String(format: "st:%02x mm:%d eda:%d a:%.2f", status, mm, instant, a))
+    }
+}
--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 HelloMoodMetric
 ===============
 
-This is a MacOS implementation of the Swift sample code from the Mood Metric
-2015 SDK document. It is updated to work with the latest version of the Swift
-programming language, as provided with XCode 7.3
+An example implementation of the Mood Metric SDK, for MacOS X and iOS,
+based on the March 2015 version of the SDK documentation. It is updated
+to work with the latest version of the Swift programming language, as
+provided with XCode 7.3
 
-In order to be able to use this project with a Mac you will need to ensure
-that your Mac supportr Bluetooth 4.0, with BLE.
+Both the MacOS X and iOS targets are designed to log to the XCode console
+and will not display anything in the application GUI.
+
+This project is not provided by the people at [MoodMetric](http://www.moodmetric.com).
+Instead, it was quickly put together during the Emotion Hack Day 2016 event,
+to help other developers at the event see how they could leverage the ring in
+the projects.
+
+
